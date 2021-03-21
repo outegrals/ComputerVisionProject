b0ec348c6e7a0071cc6286691351dc95caab41e2
Jesse <jessedmc@gmail.com>
2021-03-21 09:43:27 -0400

    Add files via upload
    This file is for grouping projection lines together. Input is Hough Lines. Output is the 3 orthogonal directions that we need to calculate vanishing points. Not complete.

--- vps.py
+++ vps.py
@@ -7,6 +7,7 @@ Created on Sat Mar 20 12:43:32 2021
 
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 listFileNames = []
 numOfImages = 30
@@ -17,15 +18,18 @@ listEdges = []
 minLineLength = 20
 maxLineGap = 5
 listLines = []
+listEuclidPoints = []
+listEuclidPointsAllImgs = []
 
+# Generate Perspective Lines
 for i in range(numOfImages):
     listFileNames.append(fileName + str(i) + '.png')
     listImages.append(cv.imread(listFileNames[i]))
     listGrayImages.append(cv.cvtColor(listImages[i], cv.COLOR_BGR2GRAY))
     listEdges.append(cv.Canny(listGrayImages[i],50,150,apertureSize = 3))
     listLines.append(cv.HoughLines(listEdges[i], 1, np.pi / 180, 100, minLineLength, maxLineGap))
-    print('222    listFileNames[',i,'] ', listFileNames[i])
-    print('222    listLines[i].shape ', listLines[i].shape)
+    #print('222    listFileNames[',i,'] ', listFileNames[i])
+    #print('222    listLines[i].shape ', listLines[i].shape)
 
     listPolarPoints = [] # (rho, theta)   for HoughLines
     for j in range(len(listLines[i])):
@@ -37,15 +41,16 @@ for i in range(numOfImages):
 
            # cv.line(img, (x0, y0), (x1, y1), (0, 0, 255), 1)
             listPolarPoints.append((listLines[i][j][k][0], listLines[i][j][k][1]))
-    print('333    len(listPolarPoints) ', len(listPolarPoints))
+    #print('333    len(listPolarPoints) ', len(listPolarPoints))
     #print('333    listPolarPoints ', listPolarPoints)
 
+    listEuclidPoints = []
     for j in range(len(listPolarPoints)):
         rho = listPolarPoints[j][0]
         theta = listPolarPoints[j][1]
         #print('444      listLines[',i,'] ', listLines[i])
-        print('444          rho ', rho)
-        print('444          theta ', theta)
+     #   print('444          rho ', rho)
+     #   print('444          theta ', theta)
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
@@ -54,13 +59,101 @@ for i in range(numOfImages):
         y1 = int(y0 + 10000 * (a))
         x2 = int(x0 - 10000 * (-b))
         y2 = int(y0 - 10000 * (a))
-        cv.line(listImages[i],(x1,y1),(x2,y2),(0,255,0),1)
+        listEuclidPoints.append((x1, y1, x2, y2))
+        #cv.line(listImages[i],(x1,y1),(x2,y2),(0,255,0),1)
+    
+    listEuclidPointsAllImgs.append(listEuclidPoints)
 
-    cv.imwrite('hougher' + str(i) + '.png', listImages[i]) 
+    #cv.imwrite('hougher' + str(i) + '.png', listImages[i]) 
     
 print('000      len(listFileNames) ', len(listFileNames))
 print('000      len(listImages) ', len(listImages))
+print('000      len(listEuclidPoints) ', len(listEuclidPoints))
+print('000      len(listEuclidPointsAllImgs) ', len(listEuclidPointsAllImgs))
 
+#print('000      listEuclidPoints ', listEuclidPoints)
+
+listGroupLines = []
+listGroupLinesAllImgs = []
+groupThreshInPixelsX = 40
+groupThreshInPixelsY = 20
+
+avgX = -1000000
+avgY = -1000000
+listAvgX = []
+listAvgY = []
+# Group similar lines together
+# Could probably use K Clustering here
+for i in range(len(listEuclidPointsAllImgs)):   # len(listEuclidPointsAllImgs)
+    print('777    len(listEuclidPointsAllImgs[', i, ']  ', len(listEuclidPointsAllImgs[i]))
+    
+    listGroupLines = []
+    for j in range(len(listEuclidPointsAllImgs[i])):
+        print('777    len(listEuclidPointsAllImgs[', i, '][',j,'] ', len(listEuclidPointsAllImgs[i][j]))
+
+        x1 = listEuclidPointsAllImgs[i][j][0]
+        y1 = listEuclidPointsAllImgs[i][j][1]
+        x2 = listEuclidPointsAllImgs[i][j][2]
+        y2 = listEuclidPointsAllImgs[i][j][3]
+        
+        if j == 0:
+            avgX = x1
+            avgY = y1
+            continue
+        # Using abs val may be an issue here
+        diffX = abs(x1 - avgX)
+        diffY = abs(y1 - avgY)
+        
+       # print('777           i ', i)
+       # print('777           x1 ', x1, ' y1 ', y1, ' x2 ', x2, ' y2 ', y2)
+       # print('777          diffX ', diffX)
+       # print('777          diffY ', diffY)
+        if diffX < groupThreshInPixelsX:
+            print('777    diffX < groupThreshInPixelsX      diffX ', diffX)
+            if diffY < groupThreshInPixelsY:
+                print('777    diffY < groupThreshInPixelsX      diffY ', diffY)
+                listGroupLines.append(listEuclidPointsAllImgs[i][j])
+                
+    listGroupLinesAllImgs.append(listGroupLines)
+            
+            
+print('888      len(listGroupLines) ', len(listGroupLines))
+print('888      len(listGroupLinesAllImgs) ', len(listGroupLinesAllImgs))
+print('888      listGroupLinesAllImgs ', listGroupLinesAllImgs)
+# Each i here corresponds to an image
+for i in range(len(listGroupLinesAllImgs)):
+    if len(listGroupLinesAllImgs[i]) == 0:
+        continue
+   
+    print('888      listGroupLinesAllImgs[', i, '] ', listGroupLinesAllImgs[i])
+
+    for j in range(len(listGroupLinesAllImgs[i])):
+       # print('999  groupys    listGroupLinesAllImgs[', i, '][', j, '] ', listGroupLinesAllImgs[i][j])
+
+        x1 = listGroupLinesAllImgs[i][j][0]
+        y1 = listGroupLinesAllImgs[i][j][1]
+        x2 = listGroupLinesAllImgs[i][j][2]
+        y2 = listGroupLinesAllImgs[i][j][3]
+        plt.scatter(x1, y1) 
+        #cv.line(listImages[i],(x1,y1),(x2,y2),(0,0,255),1)
+    #cv.imwrite('hougherFewer' + str(i) + '.png', listImages[i]) 
+       # plt.plot(x1, y1)
+        #plt.plot(x2, y2, 'ro')
+    #plt.show()
+    
+#plt.scatter(2, 3)    
+plt.show()
+        
+
+
+
+
+    
+    
+    
+    
+    
+    
 
 #img = cv.imread('C:\\Users\\jesse\\Google Drive\\School\\usf\\ComputerVision\\Group\\archive\\images\\images\\train\\clean\\31.png')
 
